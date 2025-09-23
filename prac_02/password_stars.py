87bffc87bed73fad179e86dd90570bfddb6f04a3
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-09-23 13:50:04 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,24 @@
 LENGTH = 10
-password = input("Enter password: ")
-while len(password) < LENGTH:
-    print("Password is invalid, try again")
+
+
+def main():
+    """  """
+    password = get_password()
+    print_stars(password)
+
+
+def print_stars(password):
+    """ Print stars based on length of password"""
+    print("*" * len(password))
+
+
+def get_password():
+    """ Get valid password  """
     password = input("Enter password: ")
-print("*" * len(password))
+    while len(password) < LENGTH:
+        print("Password is invalid, try again")
+        password = input("Enter password: ")
+    return password
+
+
+main()
