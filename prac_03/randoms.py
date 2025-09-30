3fa4411b5cd286fe4c29ec2ac3aed2d608afe3e9
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-09-30 13:26:54 +1000

    Add random number generator between 1 and 100

---
+++ prac_03/randoms.py
@@ -0,0 +1,18 @@
+import random
+
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+# 14 was the number seen on line 1, the lowest would be 5 and highest would be 20
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+# 5 was the number seen on line 2, the smallest would be 3 and highest would be 10, line 2 couldn't produce a 4
+
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+# 3.2741764036879886 was the number seen, the smallest would be 2.5 and highest be 5.5
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+random_number = random.randint(1, 100)
+print(random_number)
