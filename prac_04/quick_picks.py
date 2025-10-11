2965b469eb6432cf4df396e257d17100f3c435ba
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-10-11 12:24:45 +1000

    Add lottery quick pick generator that creates sorted random numbers

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,26 @@
+import random
+
+NUMBER_PER_LINE = 6
+MIN_NUMBER = 1
+MAX_NUMBER = 45
+
+
+def main():
+    number_of_quick_picks = int(input("How many quick picks? "))
+    while number_of_quick_picks < 0:
+        print("Invalid number")
+        number_of_quick_picks = int(input("How many quick picks? "))
+
+    for i in range(number_of_quick_picks):
+        quick_pick = []
+        for j in range(NUMBER_PER_LINE):
+            number = random.randint(MIN_NUMBER, MAX_NUMBER)
+            while number in quick_pick:
+                number = random.randint(MIN_NUMBER, MAX_NUMBER)
+            quick_pick.append(number)
+        quick_pick.sort()
+
+        print(" ".join(f"{number:2d}" for number in quick_pick))
+
+
+main()
