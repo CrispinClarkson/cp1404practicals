a077c9cd1894cf5d8f50e9b396ce5f0da5a2ad98
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-10-16 11:42:59 +1000

    Complete word occurrences program

--- prac_05/word_occurances.py
+++ prac_05/word_occurances.py
@@ -1,6 +1,19 @@
 """
 Word Occurrences
-Estimate: 
+Estimate: 23 minutes
 Actual:
-
 """
+
+word_to_count = {}
+text = "This is only an example text hello hi there hi there again this is still example text"
+words = text.split()
+
+for word in words:
+    frequency = word_to_count.get(word, 0)
+    word_to_count[word] = frequency + 1
+
+max_length = max(len(word) for word in words)
+
+print(text)
+for word in sorted(word_to_count):
+    print(f"{word:{max_length}} : {word_to_count[word]}")
