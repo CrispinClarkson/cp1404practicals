d32c7863dce0653ec8d04a916ebbf0ed682f6b57
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-10-16 10:55:31 +1000

    Add hex colour codes

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,4 @@
+COLOUR_CODES = {"absolutezero": "#0048ba", "acidgreen": "#b0bf1a", "aliceblue": "#f0f8ff", "amarantha": "#e52b50",
+                "amber": "#ffbf00", "amethyst": "#9966cc", "aqua": "#00ffff", "armygreen": "#4b5320", "aureolin": "#fdee00",
+                "brilliantrose": "#ff55a3"}
+
