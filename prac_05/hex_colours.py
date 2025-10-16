3e7dddd8076b76f0513938a6547efd9774c8fed0
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-10-16 11:07:14 +1000

    Add colour code identifier program

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,3 +2,10 @@ COLOUR_CODES = {"absolutezero": "#0048ba", "acidgreen": "#b0bf1a", "aliceblue": 
                 "amber": "#ffbf00", "amethyst": "#9966cc", "aqua": "#00ffff", "armygreen": "#4b5320", "aureolin": "#fdee00",
                 "brilliantrose": "#ff55a3"}
 
+colour_name = input("Enter colour name: ").lower()
+while colour_name != "":
+    try:
+        print(f"The colour code for {colour_name} is {COLOUR_CODES[colour_name]}")
+    except KeyError:
+        print(f"The colour name {colour_name} code is invalid")
+    colour_name = input("Enter colour name: ").lower()
