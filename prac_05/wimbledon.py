ab0d09426ec97cc806f7d66e41041dd992673aad
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-10-18 10:15:04 +1000

    Complete wimbledon program

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,5 +1,48 @@
 """
 Wimbledon
-Estimate:
-Actual:
+Estimate: 30 minutes
+Actual: 51 minutes
 """
+
+FILENAME = "wimbledon.csv"
+INDEX_COUNTRY = 1
+INDEX_CHAMPION = 2
+
+
+def main():
+    records = load_records(FILENAME)
+    champion_to_count, countries = process_records(records)
+    display_results(champion_to_count, countries)
+
+
+def load_records(filename):
+    records = []
+    with open(filename, "r", encoding="utf-8-sig") as input_file:
+        input_file.readline()
+        for line in input_file:
+            parts = line.strip().split(",")
+            records.append(parts)
+        return records
+
+
+def process_records(records):
+    champion_to_count = {}
+    countries = set()
+    for record in records:
+        countries.add(record[INDEX_COUNTRY])
+        try:
+            champion_to_count[record[INDEX_CHAMPION]] += 1
+        except KeyError:
+            champion_to_count[record[INDEX_CHAMPION]] = 1
+    return champion_to_count, countries
+
+
+def display_results(champion_to_count, countries):
+    print("Wimbledon Champions: ")
+    for name, count in champion_to_count.items():
+        print(f"{name} {count}")
+    print(f"These {len(countries)} countries have won Wimbledon: ")
+    print(", ".join(sorted(countries)))
+
+
+main()
