0f9c162770ff6684618d8a2ef08f8587bf2467cb
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-10-19 20:51:35 +1000

    Rename dictionary to match naming conventions

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,11 +1,11 @@
-COLOUR_CODES = {"absolutezero": "#0048ba", "acidgreen": "#b0bf1a", "aliceblue": "#f0f8ff", "amarantha": "#e52b50",
+COLOUR_TO_HEX = {"absolutezero": "#0048ba", "acidgreen": "#b0bf1a", "aliceblue": "#f0f8ff", "amarantha": "#e52b50",
                 "amber": "#ffbf00", "amethyst": "#9966cc", "aqua": "#00ffff", "armygreen": "#4b5320", "aureolin": "#fdee00",
                 "brilliantrose": "#ff55a3"}
 
 colour_name = input("Enter colour name: ").lower()
 while colour_name != "":
     try:
-        print(f"The colour code for {colour_name} is {COLOUR_CODES[colour_name]}")
+        print(f"The colour code for {colour_name} is {COLOUR_TO_HEX[colour_name]}")
     except KeyError:
         print(f"The colour name {colour_name} code is invalid")
     colour_name = input("Enter colour name: ").lower()
