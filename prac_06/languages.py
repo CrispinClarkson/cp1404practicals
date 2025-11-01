97b055e36eeef422dce849f053d8e99241cbbb5a
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-11-01 10:27:14 +1000

    Add programming language types

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,9 +1,17 @@
 """
 Languages
-Estimate:
+Estimate: 20 minutes
 Actual:
 """
+from prac_06.programming_language import ProgrammingLanguage
 
 
 def main():
-    pass
+    """"""
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+
+
+main()
