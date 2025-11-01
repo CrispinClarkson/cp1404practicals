063a7c18e2833adc584839fdf28e4519f064a6d0
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-11-01 10:34:15 +1000

    Add appropriate docstrings

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -2,16 +2,16 @@ class ProgrammingLanguage:
     """..."""
 
     def __init__(self, name, typing, reflection, year):
-        """..."""
+        """Initialise a programming language instance. """
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
-        """..."""
+        """Return True if this programming language is dynamic."""
         return self.typing == "Dynamic"
 
     def __str__(self):
-        """..."""
+        """Return string of car showing its name, fuel and odometer readings."""
         return f"{self.name} ({self.typing} Typing, Reflection={self.reflection}, First Appeared in {self.year})"
