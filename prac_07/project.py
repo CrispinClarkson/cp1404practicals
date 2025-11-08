0a10cc532603486525c4ad2787d2935b819ee69b
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-11-08 13:57:47 +1000

    Add main class structure

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,2 +1,20 @@
 class Project:
     """..."""
+    def __init__(self, name, start_date, priority, cost_estimation, completion_percentage):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimation = cost_estimation
+        self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def is_complete(self):
+        pass
+
+    def __lt__(self, other):
+        pass
