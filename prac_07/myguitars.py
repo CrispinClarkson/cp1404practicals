91df71feeae3e56f7df7e29140299654319f67f9
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-11-08 11:55:18 +1000

    Display guitars

---
+++ prac_07/myguitars.py
@@ -0,0 +1,19 @@
+from prac_07.guitar import Guitar
+
+
+def main():
+    """Guitar program"""
+    guitars = []
+    input_file = open("guitars.csv", "r")
+    input_file.readline()
+    for line in input_file:
+        parts = line.strip().split(',')
+        guitars.append(Guitar(parts[0], parts[1], parts[2]))
+    # print(guitars)
+
+    for guitar in guitars:
+        print(f"Guitar: {guitar.name:<30}, Year: {guitar.year:<6}, Cost: ${guitar.cost}")
+
+
+main()
+
