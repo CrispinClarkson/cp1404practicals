34950e486c6207c157b78a124fb7d58f3c890e83
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-11-09 11:09:58 +1000

    Add docstrings

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,6 +1,7 @@
 class Project:
-    """..."""
+    """Project class to store Project data."""
     def __init__(self, name, start_date, priority, cost_estimation, completion_percentage):
+        """Initialise the Project object."""
         self.name = name
         self.start_date = start_date
         self.priority = priority
@@ -8,14 +9,14 @@ class Project:
         self.completion_percentage = completion_percentage
 
     def __str__(self):
+        """Return a string representation of the Project."""
         return (f"{self.name}, start: {self.start_date}, priority {self.priority}, "
                 f"estimate: ${self.cost_estimation:.2f}, completion: {self.completion_percentage}%")
 
-    def __repr__(self):
-        return str(self)
-
     def is_complete(self):
+        """Return true if the project is complete, otherwise return false."""
         return self.completion_percentage == 100
 
     def __lt__(self, other):
+        """Compare projects by priority."""
         return self.priority < other.priority
