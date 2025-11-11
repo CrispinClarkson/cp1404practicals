2d932842b54f9b6b628e351dc850263b1c9b268a
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-11-11 12:47:18 +1000

    Complete box layout .py and .kv files

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -9,11 +9,12 @@ class BoxLayoutDemo(App):
         return self.root
 
     def handle_greet(self):
-        print("greet")
+        # print("greet")
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def clear_output(self):
         self.root.ids.output_label.text = ""
+        self.root.ids.input_name.text = ""
 
 
 BoxLayoutDemo().run()
