36c5811cedc5a14dc97b659a1c7db8633f224103
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-11-15 12:33:18 +1000

    Add dynamic labels programs

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.properties import StringProperty
+
+class DynamicLabelsApp(App):
+
+
+
+DynamicLabelsApp().run()
