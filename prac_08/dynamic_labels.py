ade4a5ff335dc4c67ec44832dc0708e18e0cebea
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-11-15 12:41:58 +1000

    Complete dynamic labels .py

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -1,10 +1,26 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.core.window import Window
-from kivy.properties import StringProperty
+from kivy.uix.label import Label
+
 
 class DynamicLabelsApp(App):
+    """..."""
+    def __init__(self, **kwargs):
+        """..."""
+        super().__init__(**kwargs)
+        self.names = ["Bob", "Joe", "Alice", "George", "John"]
 
+    def build(self):
+        """Build the Kivy app from the kv file"""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
+        return self.root
+
+    def create_labels(self):
+        for name in self.names:
+            dynamic_label = Label(text=name)
+            self.root.ids.main.add_widget(dynamic_label)
 
 
 DynamicLabelsApp().run()
