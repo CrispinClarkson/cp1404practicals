97ea101abafc079c1d3403ec0f4f8ae6099feb37
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-11-18 13:32:06 +1000

    Complete silver service taxi class program

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,20 @@
+from prac_09.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """Represent a silver service taxi."""
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a silver service taxi."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= self.fanciness
+
+    def get_fare(self):
+        """Get the current fare."""
+        return self.flagfall + super().get_fare()
+
+    def __str__(self):
+        """Return a string representation of the taxi."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:2f}"
