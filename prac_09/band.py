97715eb1740454f9db901620e0109bcc62313b60
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-11-22 10:55:46 +1000

    Add docstrings

--- prac_09/band.py
+++ prac_09/band.py
@@ -2,21 +2,21 @@ from prac_09.musician import Musician
 
 
 class Band:
-    """..."""
+    """Band class represents a group of musicians."""
     def __init__(self, name):
-        """..."""
+        """Initialise a Band instance."""
         self.name = name
         self.musicians = []
 
     def __str__(self):
-        """..."""
+        """Return a string representation of the band."""
         return f"{self.name} ({",".join([str(musician) for musician in self.musicians])}"
 
     def add(self, musician):
-        """..."""
+        """Add musician to the band."""
         self.musicians.append(musician)
 
     def play(self):
-        """..."""
+        """Return which musician is currently playing."""
         return "\n".join(musician.play() for musician in self.musicians)
             
