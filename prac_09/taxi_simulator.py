6eb3f79c118eb56953a99c9feb129bb3eb8ba104
Crispin Clarkson <crispin.clarkson@my.jcu.edu.au>
2025-11-23 16:47:45 +1000

    Add appropriate functions

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -14,25 +14,9 @@ def main():
     choice = input(">>> ").lower()
     while choice != "q":
         if choice == "d":
-            if current_taxi:
-                current_taxi.start_fare()
-                distance_choice = float(input("Drive how far? "))
-                current_taxi.drive(distance_choice)
-                trip_cost = current_taxi.get_fare()
-                print(f"Your {current_taxi.name} trip cost you ${trip_cost:.2f}")
-                total_bill += trip_cost
-            else:
-                print("You need to choose a taxi before you can drive")
-
+            total_bill = drive_taxi(current_taxi, total_bill)
         elif choice == "c":
-            print("Taxis available:")
-            display(taxis)
-            taxi_choice = int(input("Choose taxi: "))
-            try:
-                current_taxi = taxis[taxi_choice]
-            except IndexError:
-                print("Invalid taxi choice")
-            # print(current_taxi)
+            current_taxi = choose_taxi(taxis)
         else:
             print("Invalid option.")
         print(f"Bill to date ${total_bill:.2f}")
@@ -41,6 +25,34 @@ def main():
 
     print(f"Total trip cost ${total_bill:.2f}")
     display(taxis)
+
+
+def drive_taxi(current_taxi, total_bill):
+    """Drive taxi and bill user based on distance."""
+    if current_taxi:
+        current_taxi.start_fare()
+        distance_choice = float(input("Drive how far? "))
+        current_taxi.drive(distance_choice)
+        trip_cost = current_taxi.get_fare()
+        print(f"Your {current_taxi.name} trip cost you ${trip_cost:.2f}")
+        total_bill += trip_cost
+    else:
+        print("You need to choose a taxi before you can drive")
+    return total_bill
+
+
+def choose_taxi(taxis):
+    """Get taxi choice with exception handling."""
+    print("Taxis available:")
+    display(taxis)
+    taxi_choice = int(input("Choose taxi: "))
+
+    try:
+        return taxis[taxi_choice]
+    except IndexError:
+        print("Invalid taxi choice")
+        return None
+    # print(current_taxi)
 
 
 def display(taxis):
